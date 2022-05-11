5b449453a1d9a04c3b3a243bbb3b95521b4d74d7
Thomas Fitzgerald <thomasfitzgerald61@gmail.com>
2022-05-11 19:12:39 +1000

    addchannel

--- src/components/TeamChannelList.jsx
+++ src/components/TeamChannelList.jsx
@@ -29,14 +29,14 @@ const TeamChannelList = ({ setToggleContainer, children, error = false, loading,
                 <p className="team-channel-list__header__title">
                     {type === 'team' ? 'Channels' : 'Direct Messages'}
                 </p>
-                {/* <AddChannel 
+                <AddChannel 
                     isCreating={isCreating}
                     setIsCreating={setIsCreating}
                     setCreateType={setCreateType} 
                     setIsEditing={setIsEditing}
                     type={type === 'team' ? 'team' : 'messaging'}
                     setToggleContainer={setToggleContainer}
-                /> */}
+                /> 
             </div>
             {children}
         </div>
